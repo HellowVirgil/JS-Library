f6fd6f942d308e5bc85c3e27bb5ff24266e9608f
HellowVirgil <1135895008@qq.com>
2015-09-18 20:12:43 +0800

    add function of drag and ready

--- dom.js
+++ dom.js
@@ -7,3 +7,36 @@ function insertAfter(newElement, targetElement) {
 		parentNode.insertBefore(newElement, targetElement.nextSibling);
 	}
 }
+
+function ready(readFn) {
+	if (document.addEventListener) {
+		document.addEventListener("DOMContentLoaded", function() {
+			readyFn && readyFn();
+		});
+	} else {
+		var bReady = false;
+
+		document.attachEvent('onreadystatechange', function() {
+			if (bReady) {
+				return ;
+			}
+			if (document.readyState == "complete" || document.readyState == "interactive") {
+				bReady = true;
+				readyFn && readyFn();
+			}
+		});
+
+		function checkDoScroll() {
+			try {
+				document.documentElement.doScroll("left");
+				if (bReady) {
+					return ;
+				}
+				bReady = true;
+				ready && readyFn();
+			} catch(e) {
+				setTimeout(checkDoScroll, 1);
+			}
+		};
+	}
+};
